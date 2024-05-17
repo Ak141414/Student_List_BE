0384dcd55ea271fa0d9d0e7577aa8a8eea7dbe2c
Thamizhazhagan-Murugan <thamizhazhagan.ca@gmail.com>
2024-05-18 00:13:58 +0530

    postgres update

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,14 @@
-import mysql from "mysql2";
+const Pool = require("pg").Pool;
 
-export const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1234",
-  database: "sys",
+const connection = new Pool({
+  host: "dpg-cp3p3uvsc6pc73ftds1g-a.oregon-postgres.render.com",
+  port: 5432,
+  ssl: true,
+  user: "arivoli",
+  password: "KzrMi8HZUbaXhgK6G1i7HvrYXYglecdx",
+  database: "test_25w5",
 });
+
+module.exports = {
+  query: (text: string, params: Object) => connection.query(text, params),
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { connection } from "./db";
+const connection = require("./db");
 const cors = require("cors");
 
 const app = express();
@@ -13,40 +13,71 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.get("/students", (req: Request, res: Response) => {
-  connection.query("select * from student", (err, rows) => {
-    if (err) throw err;
+app.get("/students", async (req: Request, res: Response) => {
+  try {
+    const { rows } = await connection.query("select * from student");
     res.send(rows);
-  });
+  } catch (err) {
+    console.log("Error in fetching student data");
+    throw err;
+  }
 });
 
-app.post("/student", (req: Request, res: Response) => {
-  const studentDetails = req.body;
-  connection.query("INSERT INTO student SET ?", studentDetails, (err, rows) => {
-    if (err) throw err;
+app.post("/student", async (req: Request, res: Response) => {
+  try {
+    const studentDetails = req.body;
+    await connection.query(
+      `INSERT INTO student (name, email, phone, enroll_number, date_of_admission) VALUES ($1, $2, $3, $4, $5)`,
+      [
+        studentDetails.name,
+        studentDetails.email,
+        studentDetails.phone,
+        studentDetails.enroll_number,
+        studentDetails.date_of_admission,
+      ]
+    );
     res.send("Student added successfully!");
-  });
+  } catch (err) {
+    console.log("Error in adding student data");
+    throw err;
+  }
 });
 
-app.put("/student/:id", (req: Request, res: Response) => {
-  const studentDetails = req.body;
-  const studentId = req.params.id;
-  connection.query(
-    "UPDATE student SET ? WHERE stu_id = ?",
-    [studentDetails, studentId],
-    (err) => {
-      if (err) throw err;
-      res.send("Student updated successfully!");
-    }
-  );
+app.put("/student/:id", async (req: Request, res: Response) => {
+  try {
+    const studentDetails = req.body;
+    const studentId = req.params.id;
+    await connection.query(
+      "UPDATE student SET name=$1, email=$2, phone=$3, enroll_number=$4, date_of_admission=$5 WHERE stu_id = $6",
+      [
+        [
+          studentDetails.name,
+          studentDetails.email,
+          studentDetails.phone,
+          studentDetails.enroll_number,
+          studentDetails.date_of_admission,
+          studentId,
+        ],
+      ]
+    );
+    res.send("Student updated successfully!");
+  } catch (err) {
+    console.log("Error in updating student data");
+    throw err;
+  }
 });
 
-app.delete("/student/:id", (req: Request, res: Response) => {
-  const studentId = req.params.id;
-  connection.query("DELETE FROM student WHERE stu_id = ?", studentId, (err) => {
-    if (err) throw err;
+app.delete("/student/:id", async (req: Request, res: Response) => {
+  try {
+    const studentId = req.params.id;
+    await connection.query("DELETE FROM student WHERE stu_id = $1", [
+      studentId,
+    ]);
     res.send("Student deleted successfully!");
-  });
+  } catch (err) {
+    console.log("Error in deleting student data");
+    throw err;
+  }
 });
 
 app.listen(port, () => {
